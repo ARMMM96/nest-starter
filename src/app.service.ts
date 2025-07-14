be5089c8781c587ddffd648c7a5c4cd3bba23fe4
Ahmed Rabie <ahmedrabie@192.168.1.3>
2025-07-14 20:47:29 +0300

    App service refactor and cleanup
        - Removed redundant logs and unnecessary data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly configService: ConfigService) { }
+
   getHello() {
-    const enviromentVariable = this.configService.get<string>(`environment`);
-    console.log(`Environment Variable TEST: ${enviromentVariable} `);
     return {
       data: 'Hello World!',
-      meta: {
-        pages: 10,
-      }
     }
   }
 }
